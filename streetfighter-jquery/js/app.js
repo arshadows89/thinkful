cbaa9f0c030546f70e4aced499d012973cc81f9e
Atif M <atif089@gmail.com>
2015-01-01 17:42:49 +0100

    Added X bind to Street Fighter

--- streetfighter-jquery/js/app.js
+++ streetfighter-jquery/js/app.js
@@ -1,37 +1,43 @@
-Ja
+$(function() {
+
+  $(document).keydown(function(event) {
+    if (event.which == 88) {
+      alert("Handler for .keydown() called.");
+    }
+  })
+
+
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
-  })
-  $('.ryu-still').keydown(function(){
-  	alert( "Handler for .keydown() called." );
-  })
-  .mouseleave(function() {
+  }).mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-  .mousedown(function() {
-   playHadouken();    
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show()
-    .animate(
-      {'left': '300px'},
-      500,
-      function() {
-        $(this).hide();
-        $(this).css('left', '-167px');
-      }
-    );
-  })
-  .mouseup(function() {
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
-  });
-});
+    .mousedown(function() {
+      playHadouken();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').show();
+      $('.hadouken').finish().show()
+        .animate({
+            'left': '300px'
+          },
+          500,
+          function() {
+            $(this).hide();
+            $(this).css('left', '-167px');
+          }
+      );
+    })
+    .mouseup(function() {
+      $('.ryu-throwing').hide();
+      $('.ryu-ready').show();
+    });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+  function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+
+})
